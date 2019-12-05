f2a8f93f8c5791b325753b913c01f058a471ace3
马腾 <1163182715@qq.com>
2019-12-05 09:13:27 +0800

    去除无用代码

--- src/main.js
+++ src/main.js
@@ -11,24 +11,6 @@ import 'src/assets/css/custom.css'
 Vue.config.productionTip = false;
 Vue.use(Vuex);
 Vue.use(ElementUI);
-Vue.prototype.$confirm = ElementUI.MessageBox.confirm;
-Vue.prototype.$checkNull = (val) => {// 判断提交对象是否有空值
-  let flag = true;
-  if (JSON.stringify(val) === '{}') {
-    flag = false;
-  } else {
-    for (const item in val) {
-      if (val.hasOwnProperty(item)) {
-        if (val[item] instanceof Array && val[item].length === 0 || val[item] === '') {
-          flag = false;
-        }
-      } else {
-        flag = false;
-      }
-    }
-  }
-  return flag;
-};
 
 new Vue({
   el: '#app',
